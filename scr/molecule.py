1b0aad3c30cbf53927eaae86d86e7a0c1e3ef80d
oliveirampo <marynna.oliveira@hotmail.com>
2021-02-19 10:43:01 +0100

    doc: Add documentation to molecule module.

--- scr/molecule.py
+++ scr/molecule.py
@@ -1,9 +1,56 @@
+"""Package that handles Molecule objects.
+
+Classes:
+    Molecule
+    moleculeEncoder(json.JSONEncoder)
+
+Methods:
+    moleculeDecoder(obj)
+"""
+
+
 import json
-import sys
 
 
-class Molecule():
+class Molecule:
+    """Molecule object.
+
+    Attributes:
+        smiles: (str) SMILES string.
+        name: (str) Molecule name.
+        code: (str) Molecule code.
+        cas: (str) CAS registry number.
+        num_frags: (str, int) Number of fragments.
+        frags: (list) Array of fragments.
+        num_links: (str, int) Number of linkages.
+        links: (list) Array of linkages.
+
+        other_names: (list) List of other names associated to molecule.
+        form: (str) Molecule formula.
+        match: (boolean)
+        inchi: (str) Standard InchiKey.
+        cid_pcp: (str) CID identifier from PubChem.
+        inchi_pcp: (str) InchiKey from PubChem.
+        name_pcp: (str) Molecule mame from PubChem.
+        form_pcp: (str) Molecule formula from PubChem.
+        nC: (str, int) Number of carbon atoms.
+        atoms: (list) List of atoms.
+        topo_cod: (str) Topology code used in GROMOS building block.
+    """
+
     def __init__(self, smiles, name, code, cas, num_frags, frags, num_links, links):
+        """Constructs all the necessary attributes for this object.
+
+        :param smiles: (str) SMILES string.
+        :param name: (str) Molecule name.
+        :param code: (str) Molecule code.
+        :param cas: (str) CAS registry number.
+        :param num_frags: (str, int) Number of fragments.
+        :param frags: (list) Array of fragments.
+        :param num_links: (str, int) Number of linkages.
+        :param links: (list) Array of linkages.
+        """
+
         self.smiles = smiles
         self.name = name
         self.enu_name = name
@@ -33,7 +80,31 @@ class Molecule():
 
 
 class moleculeEncoder(json.JSONEncoder):
+    """JSON <http://json.org> encoder for Molecule data structure.
+        Returns a serializable object.
+
+        Methods:
+            def default(obj)
+        """
+
     def default(self, obj):
+        """Returns a serializable object for ``o``, or calls the base implementation
+        (to raise a ``TypeError``).
+
+        For example, to support arbitrary iterators, you could
+        implement default like this::
+
+            def default(self, o):
+                try:
+                    iterable = iter(o)
+                except TypeError:
+                    pass
+                else:
+                    return list(iterable)
+                # Let the base class default method raise the TypeError
+                return JSONEncoder.default(self, o)
+        """
+
         if isinstance(obj, Molecule):
             return {
                 '__type__': 'Molecule',
@@ -63,6 +134,13 @@ class moleculeEncoder(json.JSONEncoder):
 
 
 def moleculeDecoder(obj):
+    """Method to decode a Molecule object.
+
+    :param obj:
+    :return:
+        mol: (Molecule)
+    """
+
     if '__type__' in obj and obj['__type__'] == 'Molecule':
         mol = Molecule(obj['smiles'], obj['name'], obj['code'], obj['cas'], obj['num_frags'], obj['frags'], obj['num_links'], obj['links'])
         mol.enu_name = obj['enu_name']
@@ -81,6 +159,3 @@ def moleculeDecoder(obj):
         return mol
     else:
         return obj
-#
-
-
