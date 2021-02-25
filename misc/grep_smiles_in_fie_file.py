2641ea4b04ae39eb94f7d77b2450b0366aa2342d
oliveirampo <marynna.oliveira@hotmail.com>
2021-02-25 19:18:05 +0100

    feat: Add module with auxiliary packages.
    Package added: grep SMILES listed in molList file from fie file.

---
+++ misc/grep_smiles_in_fie_file.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import sys
+import os
+
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+
+from scr import IO
+from scr import utils
+
+
+
+
+def main():
+    """Greps the SMILES strings listed in molList file from the fie file.
+
+    :return:
+    """
+
+    molListFile = sys.argv[1]
+    fieFile = sys.argv[2]
+    outFile = sys.argv[3]
+
+    isomers = IO.readFieFile(fieFile)
+    isomers = utils.canonicalizeSmiles(isomers)
+
+    molList = pd.read_csv(molListFile, sep='\s+',
+                         names=['code', 'formula', 'cas', 'name', 'inchi', 'smiles', 'K', 'X', 'Y', 'Z'])
+    molList = utils.canonicalizeSmiles(molList)
+
+    df = pd.merge(molList, isomers, on='smiles')
+
+    if df.shape[0] != molList.shape[0]:
+        sys.exit('ERROR: Some SMILES were not matched.')
+
+    columns = ['nam', 'frm', 'smiles']
+    df.to_csv(outFile, columns=columns, index=False, header=False, sep='\t')
+
+
+if __name__ == '__main__':
+    main()
